c2a7057a7686a269abb71bf1cbc3c11c52e45de5
wangsong <384097785@qq.com>
2023-07-04 00:01:14 +0800

    Initial commit

--- trainresnet.py
+++ trainresnet.py
@@ -20,7 +20,7 @@ from model import resnet34
 
 
 def main():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
     print("using {} device.".format(device))
     
     data_transform = {
